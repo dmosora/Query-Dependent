96f251071a3fd18cd5726563cc2a462b34117980
Dan Mosora <moso361@gmail.com>
2011-10-17 10:24:14 -0400

    Working on the MapWidget, added a few classes that may or may not be used in the end.

--- src/Visualization/MapWidget.h
+++ src/Visualization/MapWidget.h
@@ -2,6 +2,31 @@
 #define MAPWIDGET_H
 
 #include <QWidget>
+#include <QtWebkit/QGraphicsWebView>
+
+// This will help us draw and keep track of the plane icon.
+class AircraftOverlay
+{
+public:
+    AircraftOverlay() {}
+};
+
+// This will set up the background map area with a url request to Google Maps' API
+class MapArea : public QGraphicsWebView
+{
+public:
+    explicit MapArea(QWidget *parent = 0);
+
+public slots:
+
+private:
+    double _latStart;
+    double _lonStart;
+    double _latEnd;
+    double _lonEnd;
+
+    double _url;
+};
 
 class MapWidget : public QWidget
 {
@@ -13,6 +38,8 @@ signals:
 
 public slots:
 
+private:
+    MapArea* _mapArea;
 };
 
 #endif // MAPWIDGET_H
