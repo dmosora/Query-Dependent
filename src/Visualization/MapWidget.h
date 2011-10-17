1903d52ae32d6f177e0eeeaa715a29ee79c39acc
moso361@gmail.com <dmosora@dm-OSWindows.(none)>
2011-10-17 02:29:59 -0400

    Added base MapWidget class to begin work on map vis, added images for plane icon, began structure for setting up widgets for the vis.

---
+++ src/Visualization/MapWidget.h
@@ -0,0 +1,18 @@
+#ifndef MAPWIDGET_H
+#define MAPWIDGET_H
+
+#include <QWidget>
+
+class MapWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit MapWidget(QWidget *parent = 0);
+
+signals:
+
+public slots:
+
+};
+
+#endif // MAPWIDGET_H
